e0b7290920738649dbddbf09622268e0c04625c6
afwilson42 <andrewf.wilson@bellevuecollege.edu>
2021-06-24 16:59:06 -0700

    basic MVC app

---
+++ AndrewFPsol/AndrewFP/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AndrewFP.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AndrewFPsol/AndrewFP/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AndrewFP.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
